414c56e0f1ca9db8cd1abab350f28347d24dd867
Szabolcs Petkes <sz.petkes@gmail.com>
2021-09-06 13:36:22 +0200

    Create test_k3.py

---
+++ testproject/test_k3.py
@@ -0,0 +1,74 @@
+"""""""""
+Készíts egy Python python applikációt (egy darab python file) ami selenium-ot használ.
+
+A program töltse be a Alfanumerikus mezőapp-ot az https://ambitious-sky-0d3acbd03.azurestaticapps.net/k3.html oldalról.
+
+Feladatod, hogy automatizáld selenium webdriverrel a Alfanumerikus mező app tesztelését.
+
+Az ellenőrzésekhez használj pytest keretrendszert. A tesztjeidben használj assert összehasonlításokat használj!
+
+A cél a mező validáció tesztelése:
+
+    Helyes kitöltés esete:
+        title: abcd1234
+        Nincs validációs hibazüzenet
+
+    Illegális karakterek esete:
+        title: teszt233@
+        Only a-z and 0-9 characters allewed.
+
+    Tul rövid bemenet esete:
+        title: abcd
+        Title should be at least 8 characters; you entered 4.
+"""
+
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+
+@pytest.fixture(scope='session')
+def browser():
+    driver = webdriver.Chrome
+    driver.get("https://ambitious-sky-0d3acbd03.azurestaticapps.net/k3.html")
+    return driver
+
+
+driver.get("https://ambitious-sky-0d3acbd03.azurestaticapps.net/k3.html")
+time.sleep(1)
+
+# elements
+title1 = 'abcd1234'
+title2 = 'teszt233@'
+title3 = 'abcd'
+
+text = driver.find_element_by_id("title")
+error = driver.find_element_by_xpath('/html/body/form/span')
+
+# TC1
+# valid data
+text.send_keys(title1)
+assert error.is_enabled()
+
+# TC2
+# invalid data
+text.clear()
+text.send_keys(title2)
+assert error.text == 'Only a-z and 0-9 characters allewed'
+
+# TC3
+# short data
+text.clear()
+text.send_keys(title3)
+assert error.text == 'Title should be at least 8 characters; you entered 4.'
+
+driver.close()
